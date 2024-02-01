00e8af06bf0b29a5ac4dded8c441afa8c5a9ebac
Ann Vuong <annvuong@Anns-MBP-M1.local>
2024-02-01 14:54:01 -0800

    I updated the name you asked

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 console.log("this is your intro to git")
 console.log("Hello world")
 
-var landingSpot = document.getElementById("jason_spot")
+var landingSpot = document.getElementById("numberlanding_spot")
 console.log("landing spot", landingSpot)
 landingSpot.innerHTML = "12345678910"
